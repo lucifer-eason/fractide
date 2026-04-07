56199fde91fcbd3e5cb3ed475863deb906ae5116
Async <lucifer.eason.justin@gmail.com>
2026-04-07 21:57:21 +0800

    test2

---
+++ app.js
@@ -0,0 +1,40 @@
+let allPosts = [];
+
+async function loadPosts() {
+  const res = await fetch('posts/manifest.json');
+  allPosts = await res.json();
+  renderPosts(allPosts);
+}
+
+function renderPosts(list) {
+  const container = document.getElementById('posts');
+  container.innerHTML = '';
+
+  list.forEach(post => {
+    const div = document.createElement('div');
+    div.className = 'card';
+
+    div.innerHTML = `
+      <div class="title">${post.title}</div>
+      <div class="desc">${post.desc}</div>
+    `;
+
+    div.onclick = () => {
+      window.location.href = `post.html?file=${post.file}`;
+    };
+
+    container.appendChild(div);
+  });
+}
+
+function searchPosts() {
+  const value = document.getElementById('search').value.toLowerCase();
+  renderPosts(allPosts.filter(p => p.title.toLowerCase().includes(value)));
+}
+
+function filterPosts(category) {
+  if (category === 'all') return renderPosts(allPosts);
+  renderPosts(allPosts.filter(p => p.category === category));
+}
+
+loadPosts();
